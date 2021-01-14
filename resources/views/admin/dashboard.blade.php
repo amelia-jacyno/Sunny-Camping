60d5d9777f1c2b9b71b47a9d24554a44b9c56448
amelia-jacyno <ameliajacyno@gmail.com>
2021-01-14 19:53:03 +0100

    feat: install ApexCharts for Vue

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,9 @@
 @extends('layouts.admin')
+
+@section('main')
+    <div id="app">
+        <div id="chart">
+            <apexchart type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
+        </div>
+    </div>
+@endsection
