86a3cc7bebf14e9b416feff2e835f1f89ca91641
amelia-jacyno <ameliajacyno@gmail.com>
2021-01-14 18:59:52 +0100

    feat: install Vue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,6 @@
 window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
+window.Vue = require('vue');
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
