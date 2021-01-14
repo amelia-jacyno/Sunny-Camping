97cad98a19ab0b4e6caf42464ca3a69a9d4dee7f
amelia-jacyno <ameliajacyno@gmail.com>
2021-01-15 00:14:15 +0100

    feat: make a Vue object with temporary data

---
+++ resources/js/admin/clients.js
@@ -0,0 +1,12 @@
+const clientsTable = new Vue({
+    el: '#clients-table',
+    data: {
+        clients: [
+            {id: 1},
+            {id: 2},
+            {id: 3},
+            {id: 4},
+            {id: 5},
+        ]
+    }
+});
