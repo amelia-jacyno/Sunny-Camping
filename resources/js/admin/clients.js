bb1771700646613a2d73f062086c51894b729d81
amelia-jacyno <ameliajacyno@gmail.com>
2021-01-15 16:43:12 +0100

    feat: Vuetable-2 cleanup

--- resources/js/admin/clients.js
+++ resources/js/admin/clients.js
@@ -1,9 +1,7 @@
-import clientsTableRow from '../../components/clientsTableRow';
 import ClientsTable from "../../components/ClientsTable";
 
-Vue.component('clients-table-row', clientsTableRow);
 Vue.component('clients-table', ClientsTable);
 
-window.clientsTable = new Vue({
+new Vue({
     el: '#clients-table',
 });
