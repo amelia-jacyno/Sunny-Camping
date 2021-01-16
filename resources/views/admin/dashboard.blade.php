99041091372a00ad75bf4935e97b0d2656b18c96
amelia-jacyno <ameliajacyno@gmail.com>
2021-01-16 20:24:13 +0100

    ref: remove attempt to load apexcharts component which is not imported

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,8 +2,5 @@
 
 @section('main')
     <div id="app">
-        <div id="chart">
-            <apexchart type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
-        </div>
     </div>
 @endsection
