00e47442a92218aee74ddf7b781bed8195949879
Adam Jacyno <jacynoadam@gmail.com>
2021-01-16 20:22:12 +0100

    style: remove unused ClientsRepository

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,20 +3,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Repositories\ClientRepositoryInterface;
-
 class AdminController extends Controller
 {
-    /**
-     * @var ClientRepositoryInterface
-     */
-    private $clientsRepository;
-
-    public function __construct(ClientRepositoryInterface $clientsRepository)
-    {
-        $this->clientsRepository = $clientsRepository;
-    }
-
     public function dashboard()
     {
         return view('admin.dashboard', ['page' => 'dashboard', 'nav_items' =>
@@ -25,9 +13,8 @@ class AdminController extends Controller
 
     public function clients()
     {
-        $clients = $this->clientsRepository->all();
         return view('admin.clients', ['page' => 'clients', 'nav_items' =>
-            config('constants.admin_nav_items'), 'clients' => $clients]);
+            config('constants.admin_nav_items')]);
     }
 
     public function bills()
