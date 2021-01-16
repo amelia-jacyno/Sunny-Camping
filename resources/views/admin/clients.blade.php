3289f0010ffeb66d81d45b1e8e792d6046a62f6c
amelia-jacyno <ameliajacyno@gmail.com>
2021-01-16 20:23:19 +0100

    style: remove unused HTML from a template

--- resources/views/admin/clients.blade.php
+++ resources/views/admin/clients.blade.php
@@ -13,31 +13,6 @@
     </div>
 @endsection
 
-@section('temp')
-    <table class="table table-responsive-lg table-bordered text-center mt-3">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Imię i Nazwisko</th>
-            <th scope="col">Data Przyjazdu</th>
-            <th scope="col">Data Odjazdu</th>
-            <th scope="col">Sektor</th>
-            <th scope="col">Osoby</th>
-            <th scope="col">Prąd</th>
-            <th scope="col">Miejsca</th>
-            <th scope="col">Rabat</th>
-            <th scope="col">Cena</th>
-            <th scope="col">Komentarz</th>
-            <th scope="col">Opcje</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="client in clients" :key="client.id" :client="client" is="clients-table-row">
-        </tr>
-        </tbody>
-    </table>
-@endsection
-
 @section('main')
     @yield('options')
     @yield('table')
