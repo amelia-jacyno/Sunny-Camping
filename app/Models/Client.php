5b78f0cf79bd30cb05ad076411502072a2ea8cbf
Adam Jacyno <jacynoadam@gmail.com>
2021-01-18 18:59:24 +0100

    build: change the architecture to support camelCase for Models

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,14 +3,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use DateTime;
-use Illuminate\Http\Request;
-use phpDocumentor\Reflection\Types\Boolean;
-use function PHPUnit\Framework\isNull;
-
-
-class Client extends Model
+class Client extends BaseModel
 {
     protected $guarded = [];
 }
--- resources/components/ClientsForm.vue
+++ resources/components/ClientsForm.vue
@@ -2,25 +2,25 @@
     <form id="client-form" class="row mt-4" @submit.prevent="submitClientForm()" method="POST" action="">
         <div class="col-6 col-sm-4 col-md-3 form-group">
             <label for="first_name">Imię</label>
-            <input v-model="client.first_name" @blur="client.first_name = trim(client.first_name)"
+            <input v-model="client.firstName" @blur="client.firstName = trim(client.firstName)"
                    name="first_name"
                    type="text" placeholder="Imię"
                    class="form-control form-control-sm">
         </div>
         <div class="col-6 col-sm-4 col-md-3 form-group">
             <label for="last_name">Imię</label>
-            <input v-model="client.last_name" @blur="client.last_name = trim(client.last_name)" name="last_name"
+            <input v-model="client.lastName" @blur="client.lastName = trim(client.lastName)" name="last_name"
                    type="text" placeholder="Naziwsko"
                    class="form-control form-control-sm">
         </div>
         <div class="col-6 col-sm-4 col-md-3 form-group">
             <label for="arrival_date">Data przyjazdu</label>
-            <input v-model="client.arrival_date" @change="updateDiscount()" name="arrival_date" type="date"
+            <input v-model="client.arrivalDate" @change="updateDiscount()" name="arrival_date" type="date"
                    class="form-control form-control-sm">
         </div>
         <div class="col-6 col-sm-4 col-md-3 form-group">
             <label for="departure_date">Data odjazdu</label>
-            <input v-model="client.departure_date" @change="updateDiscount()" name="departure_date" type="date"
+            <input v-model="client.departureDate" @change="updateDiscount()" name="departure_date" type="date"
                    class="form-control form-control-sm"
                    required>
         </div>
@@ -41,12 +41,12 @@
         </div>
         <div class="col-6 col-sm-4 col-md-3 form-group">
             <label for="small_places">Małe miejsca</label>
-            <input v-model="client.small_places" name="small_places" type="number" placeholder="0"
+            <input v-model="client.smallPlaces" name="small_places" type="number" placeholder="0"
                    class="form-control form-control-sm">
         </div>
         <div class="col-6 col-sm-4 col-md-3 form-group">
             <label for="big_places">Duże miejsca</label>
-            <input v-model="client.big_places" name="big_places" type="number" placeholder="0"
+            <input v-model="client.bigPlaces" name="big_places" type="number" placeholder="0"
                    class="form-control form-control-sm">
         </div>
         <div class="col-6 col-sm-4 col-md-3 form-group">
@@ -76,16 +76,16 @@
         data() {
             return {
                 client: {
-                    first_name: null,
-                    last_name: null,
-                    arrival_date: null,
-                    departure_date: null,
+                    firstName: null,
+                    lastName: null,
+                    arrivalDate: null,
+                    departureDate: null,
                     sector: null,
                     adults: null,
                     children: null,
                     electricity: null,
-                    small_places: null,
-                    big_places: null,
+                    smallPlaces: null,
+                    bigPlaces: null,
                     comment: null,
                     discount: 0
                 }
@@ -96,6 +96,7 @@
                 axios.get(baseUrl + '/admin/clients/find-json/' + this.id)
                     .then((response) => {
                         this.client = response.data;
+
                     })
             }
         },
@@ -105,17 +106,17 @@
                 return null;
             },
             updateDiscount() {
-                if (!this.client.arrival_date || !this.client.departure_date) return false;
-                let days = (new Date(this.client.departure_date) - new Date(this.client.arrival_date)) / (1000 * 60 * 60 * 24);
+                if (!this.client.arrivalDate || !this.client.departureDate) return false;
+                let days = (new Date(this.client.departureDate) - new Date(this.client.arrivalDate)) / (1000 * 60 * 60 * 24);
                 console.log(days);
                 if (days >= 7 && days < 14) this.client.discount = 5;
                 else if (days >= 14) this.client.discount = 10;
                 else this.client.discount = 0;
             },
             submitClientForm() {
-                if (this.client.first_name) this.client.first_name = this.client.first_name.trim();
-                if (this.client.last_name) this.client.last_name = this.client.last_name.trim();
-                if (!this.client.first_name && !this.client.last_name) {
+                if (this.client.firstName) this.client.firstName = this.client.firstName.trim();
+                if (this.client.lastName) this.client.lastName = this.client.lastName.trim();
+                if (!this.client.firstName && !this.client.lastName) {
                     alert("Imię lub Nazwisko musi być wpisane!");
                     return false;
                 }
@@ -123,12 +124,12 @@
                     alert("Musisz wpisać co najmniej jedną osobę!");
                     return false;
                 }
-                if (new Date(this.client.arrival_date) >= new Date(this.client.departure_date)) {
+                if (new Date(this.client.arrivalDate) >= new Date(this.client.departureDate)) {
                     alert("Data odjazdu musi być później od daty przyjazdu!");
                     return false;
                 }
-                if (this.client.adults < 0 || this.client.children < 0 || this.client.electricity < 0 || this.client.big_places < 0 ||
-                    this.client.small_places < 0) {
+                if (this.client.adults < 0 || this.client.children < 0 || this.client.electricity < 0 || this.client.bigPlaces < 0 ||
+                    this.client.smallPlaces < 0) {
                     alert("Liczby nie mogą być ujemne!")
                     return false;
                 }
