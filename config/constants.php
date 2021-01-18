9a477f0cac387a99f8505390bd4d5172141617d1
amelia-jacyno <ameliajacyno@gmail.com>
2021-01-18 23:28:34 +0100

    ref: clean constants config file and load accepted discount values from it

--- config/constants.php
+++ config/constants.php
@@ -6,17 +6,5 @@ return [
         'clients' => "Klienci",
         'bills' => "Rachunki"
     ],
-    'client_inputs' => [
-        ['name' => 'first_name', 'type' => 'text', 'label' => 'Imię', 'placeholder' => 'Imię'],
-        ['name' => 'last_name', 'type' => 'text', 'label' => 'Nazwisko', 'placeholder' => 'Nazwisko'],
-        ['name' => 'arrival_date', 'type' => 'date', 'label' => 'Data przyjazdu', 'required' => true],
-        ['name' => 'departure_date', 'type' => 'date', 'label' => 'Data odjazdu', 'required' => true],
-        ['name' => 'adults', 'type' => 'number', 'label' => 'Dorośli', 'placeholder' => 0],
-        ['name' => 'children', 'type' => 'number', 'label' => 'Dzieci', 'placeholder' => 0],
-        ['name' => 'electricity', 'type' => 'number', 'label' => 'Prąd', 'placeholder' => 0],
-        ['name' => 'small_places', 'type' => 'number', 'label' => 'Małe miejsca', 'placeholder' => 0],
-        ['name' => 'big_places', 'type' => 'number', 'label' => 'Duże miejsca', 'placeholder' => 0],
-        ['name' => 'comment', 'type' => 'text', 'label' => 'Komentarz', 'placeholder' => 'Komentarz'],
-        ['name' => 'discount', 'type' => 'select', 'label' => 'Rabat', 'options' => [0 => '0%', 5 => '5%', 10 => '10%']]
-    ]
+    'discounts' => [0, 5, 10]
 ];
