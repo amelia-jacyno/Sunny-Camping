7b631b2cfb5fd1a427968c168991e398046e3ecb
amelia-jacyno <ameliajacyno@gmail.com>
2021-01-19 02:14:31 +0100

    feat: pass data to SettleModal

---
+++ resources/components/SettleModal.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="p-2">
+            <h1 class="text-center">{{data.firstName + " " + data.lastName}}</h1>
+        </div>
+        <div class="row no-gutters">
+            <div class="col p-1">
+                <a class="btn btn-outline-secondary w-100 h-100">Anuluj</a>
+            </div>
+            <div class="col p-1">
+                <a class="btn btn-outline-secondary w-100 h-100">Rozlicz</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['data']
+    }
+</script>
