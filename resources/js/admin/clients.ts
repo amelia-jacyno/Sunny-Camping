20ecd8215672b2e86cc88341900af6ec097613ea
Adam Jacyno <jacynoadam@gmail.com>
2021-01-29 00:54:43 +0100

    build: set up typescript with basic config

--- resources/js/admin/clients.ts
+++ resources/js/admin/clients.ts
@@ -1,6 +1,7 @@
-import ClientsTable from "../../components/ClientsTable";
-import ClientsForm from "../../components/ClientsForm";
+import ClientsTable from "../../components/ClientsTable.vue";
+import ClientsForm from "../../components/ClientsForm.vue";
 import VModal from 'vue-js-modal/dist/index.nocss.js';
+import Vue from "vue";
 
 Vue.component('clients-table', ClientsTable);
 Vue.component('clients-form', ClientsForm);
