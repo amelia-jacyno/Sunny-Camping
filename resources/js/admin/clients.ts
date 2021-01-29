41731e99514b64222c843bd7e1b96135fa9d46d8
amelia-jacyno <ameliajacyno@gmail.com>
2021-01-29 01:19:36 +0100

    build: make a file for declarations and declare non-typescript libraries there

--- resources/js/admin/clients.ts
+++ resources/js/admin/clients.ts
@@ -1,7 +1,7 @@
+import Vue from "vue";
 import ClientsTable from "../../components/ClientsTable.vue";
 import ClientsForm from "../../components/ClientsForm.vue";
 import VModal from 'vue-js-modal/dist/index.nocss.js';
-import Vue from "vue";
 
 Vue.component('clients-table', ClientsTable);
 Vue.component('clients-form', ClientsForm);
