88801514f72e0adc1a54546a575bc13be875eb3d
amelia-jacyno <ameliajacyno@gmail.com>
2021-02-08 19:37:35 +0100

    ref: remove unnecessary console.log call and reindent files

--- resources/components/VuetablePagination.vue
+++ resources/components/VuetablePagination.vue
@@ -39,9 +39,9 @@
 </template>
 
 <script>
-    import VuetablePaginationMixin from "vuetable-2/src/components/VuetablePaginationMixin";
+import VuetablePaginationMixin from "vuetable-2/src/components/VuetablePaginationMixin";
 
-    export default {
-        mixins: [VuetablePaginationMixin],
-    };
+export default {
+    mixins: [VuetablePaginationMixin],
+};
 </script>
