0a0c834803bcf0ff250942b06752a969f1e89344
amelia-jacyno <ameliajacyno@gmail.com>
2021-02-08 19:15:48 +0100

    Revert "Use TypeScript instead of JavaScript"

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.ts('resources/js/app.ts', 'public/js/app.js')
+mix.js('resources/js/app.js', 'public/js/app.js')
     .vue()
     .sass('resources/css/app.scss', 'public/css/app.css')
     .js('resources/js/parallax.min.js', 'public/js/parallax.min.js')
