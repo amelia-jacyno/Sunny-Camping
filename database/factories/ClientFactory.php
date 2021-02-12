eba831eb4095b208eb3496315a03760022b1d1a0
amelia-jacyno <ameliajacyno@gmail.com>
2021-02-12 17:16:49 +0100

    feat: implement ClientFactory definition

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -23,8 +23,24 @@ class ClientFactory extends Factory
      */
     public function definition()
     {
+        $arrivalDate = $this->faker->dateTimeBetween('-2 months')->format('Y-m-d');
         return [
-            //
+            "first_name" => $this->faker->firstName,
+            "last_name" => $this->faker->lastName,
+            "arrival_date" => $arrivalDate,
+            "departure_date" => $arrivalDate->add(new DateInterval('P'.rand(1, 30).'D')),
+            "sector" => rand(1, 6),
+            "adults" => rand(1, 5),
+            "children" => rand(0,5),
+            "electricity" => rand(0, 1),
+            "small_places" => rand(0, 5),
+            "big_places" => rand(0, 5),
+            "discount" => array_rand([0, 5, 10]),
+            "comment" => $this->faker->sentence(),
+            "status" => 'unsettled',
+            "paid" => 0,
+            "created_at" => now(),
+            "updated_at" => now()
         ];
     }
 }
