83caa11db5b5e22a3aed12e99ecf846d9b9c31ea
amelia-jacyno <ameliajacyno@gmail.com>
2021-02-25 23:58:41 +0100

    ref: add new clients migration file

---
+++ database/migrations/2021_02_25_223558_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 32);
+            $table->date('arrival_date')->nullable();
+            $table->date('departure_date')->nullable();
+            $table->string('comment')->nullable();
+            $table->tinyInteger('discount')->default(0);
+            $table->double('paid', 8, 2)->default(0);
+            $table->string('status', 16);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
