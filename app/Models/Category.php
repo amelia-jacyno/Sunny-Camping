2112721e2f33ddf303d48f4d31a11eda14c52fa5
jacynoadam <jacynoadam@gmail.com>
2021-03-06 19:57:44 +0100

    SC-5 make initial changes to support categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Category extends BaseModel
 {
     use HasFactory;
+
+    public function categoryItems()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
