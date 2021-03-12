12dc60b038033aaa9ceb1dbe1af3209df49fcbf5
amelia-jacyno <ameliajacyno@gmail.com>
2021-03-12 22:01:39 +0100

    SC-5 update url and dependencies

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,7 +12,7 @@ window.VueApexCharts = require('vue-apexcharts');
 
 window.axios = require('axios');
 
-window.baseUrl = 'http://reservation-manager.local';
+window.baseUrl = 'http://sunnycamping.local';
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
