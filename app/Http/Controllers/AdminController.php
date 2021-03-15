69e54c76104ef5ce2cd4d70314d099d454af02b3
jacynoadam <jacynoadam@gmail.com>
2021-03-15 01:25:54 +0100

    SC-54 install php-cs-fixer and fix files

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,25 +1,21 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
 class AdminController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard', ['page' => 'dashboard', 'nav_items' =>
-            config('constants.admin_nav_items')]);
+        return view('admin.dashboard', ['page' => 'dashboard', 'nav_items' => config('constants.admin_nav_items')]);
     }
 
     public function clients()
     {
-        return view('admin.clients', ['page' => 'clients', 'nav_items' =>
-            config('constants.admin_nav_items')]);
+        return view('admin.clients', ['page' => 'clients', 'nav_items' => config('constants.admin_nav_items')]);
     }
 
     public function bills()
     {
-        return view('admin.bills', ['page' => 'bills', 'nav_items' =>
-            config('constants.admin_nav_items')]);
+        return view('admin.bills', ['page' => 'bills', 'nav_items' => config('constants.admin_nav_items')]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ClientSeeder::class
+            ClientSeeder::class,
         ]);
     }
 }
