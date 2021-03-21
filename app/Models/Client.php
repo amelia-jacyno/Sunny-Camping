4b7284f7a5192a9c9ae251a4efdbcdc9586f4b15
amelia-jacyno <ameliajacyno@gmail.com>
2021-03-22 00:54:48 +0100

    SC-52 move all custom model properties to the model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,12 +2,49 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Client extends BaseModel
 {
     use HasFactory;
-    protected $guarded = [];
+
+    protected $guarded = ['price', 'price_per_day', 'days'];
+
+    protected $appends = ['price', 'price_per_day', 'days'];
+
+    public function getPriceAttribute()
+    {
+        $days = $this->days;
+        if ($days == 0) {
+            return 0;
+        }
+
+        return floor($days * $this->pricePerDay * (100 - $this->discount) / 100);
+    }
+
+    public function getPricePerDayAttribute()
+    {
+        $clientItems = $this->clientItems;
+        $price = 0;
+        foreach ($clientItems as $clientItem) {
+            $price += $clientItem->price * $clientItem->count;
+        }
+
+        return $price;
+    }
+
+    public function getDaysAttribute()
+    {
+        if (!strtotime($this->arrival_date) || !strtotime($this->departure_date)) {
+            return 0;
+        }
+
+        $arrival = new Carbon($this->arrival_date);
+        $departure = new Carbon($this->departure_date);
+
+        return $departure->diff($arrival)->format('%a');
+    }
 
     public function clientItems()
     {
