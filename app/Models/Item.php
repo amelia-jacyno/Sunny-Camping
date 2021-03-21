60750e2103cd280e09f4b4de027317075a655f32
amelia-jacyno <ameliajacyno@gmail.com>
2021-03-21 22:43:54 +0100

    SC-61 add laravel ide helper and generate phpdocs

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,8 +2,33 @@
 
 namespace App\Models;
 
+use Database\Factories\ItemFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Item.
+ *
+ * @property int $id
+ * @property string $category_id
+ * @property string $name
+ * @property float $price
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static ItemFactory factory(...$parameters)
+ * @method static Builder|Item newModelQuery()
+ * @method static Builder|Item newQuery()
+ * @method static Builder|Item query()
+ * @method static Builder|Item whereCategoryId($value)
+ * @method static Builder|Item whereCreatedAt($value)
+ * @method static Builder|Item whereId($value)
+ * @method static Builder|Item whereName($value)
+ * @method static Builder|Item wherePrice($value)
+ * @method static Builder|Item whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Item extends BaseModel
 {
     use HasFactory;
