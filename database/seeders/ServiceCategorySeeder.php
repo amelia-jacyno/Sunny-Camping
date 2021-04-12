9cf0c363961e90b8e152726c7b5bd7c272e6765b
Pawel Jacyno <pjacyn@gmail.com>
2021-04-12 21:31:35 +0000

    fix: Voyager install and seeds

--- database/seeders/ServiceCategorySeeder.php
+++ database/seeders/ServiceCategorySeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class CategorySeeder extends Seeder
+class ServiceCategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
