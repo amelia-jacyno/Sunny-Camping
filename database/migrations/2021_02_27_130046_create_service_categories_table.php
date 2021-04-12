d28cd650bc458923b99d51208ccc984b22d34b22
Pawel Jacyno <pjacyn@gmail.com>
2021-04-12 21:31:35 +0000

    fix: Voyager install and seeds

--- database/migrations/2021_02_27_130046_create_service_categories_table.php
+++ database/migrations/2021_02_27_130046_create_service_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateServiceCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('service_categories', function (Blueprint $table) {
             $table->id();
             $table->integer('service_id');
             $table->string('name', 32);
