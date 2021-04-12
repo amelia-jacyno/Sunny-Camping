1abc758933209b20783cb092b28c1fdbabb86489
PJ <pjacyn@gmail.com>
2021-04-12 20:31:03 +0200

    feat: remove nav menu

--- config/constants.php
+++ config/constants.php
@@ -1,10 +1,5 @@
 <?php
 
 return [
-    'admin_nav_items' => [
-        'dashboard' => 'Podsumowanie',
-        'clients' => 'Klienci',
-        'bills' => 'Rachunki',
-    ],
     'discounts' => [0, 5, 10],
 ];
