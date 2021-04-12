0bb4b12e2f88a3fb599c33d087e1ba8e7972de35
PJ <pjacyn@gmail.com>
2021-04-12 20:31:54 +0200

    feat: move dashboard to voyager panel

--- routes/web.php
+++ routes/web.php
@@ -20,20 +20,22 @@ use TCG\Voyager\Facades\Voyager;
 Route::get('/{year?}', [HomeController::class, 'home'])->where('year', '[0-9]+')->name('home');
 
 Route::group(['prefix' => '/admin', 'middleware' => 'auth'], function () {
-    Route::prefix('/clients')->group(function () {
-        Route::get('/add-client', [ClientController::class, 'addClient']);
-        Route::get('/edit/{id}', [ClientController::class, 'edit'])->name('admin.clients.edit');
-    });
-    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-    Route::get('/clients', [AdminController::class, 'clients'])->name('admin.clients');
-    Route::get('/bills', [AdminController::class, 'bills']);
-    Route::redirect('/', 'admin/dashboard');
+
 });
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'cms'], function () {
+Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
+    Route::group(['middleware' => 'admin.user'], function () {
+        Route::prefix('/clients')->group(function () {
+            Route::get('/add-client', [ClientController::class, 'addClient']);
+            Route::get('/edit/{id}', [ClientController::class, 'edit'])->name('admin.clients.edit');
+        });
+        Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+        Route::get('/clients', [AdminController::class, 'clients'])->name('admin.clients');
+        Route::get('/bills', [AdminController::class, 'bills']);
+    });
 });
