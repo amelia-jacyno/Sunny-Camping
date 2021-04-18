982e6aa49f15bd10fb31f630093749b67a6f89ab
jacynoadam <jacynoadam@gmail.com>
2021-04-18 19:03:09 +0200

    fix baseUrl not being taken from .env

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,8 +12,6 @@ window.VueApexCharts = require('vue-apexcharts');
 
 window.axios = require('axios');
 
-window.baseUrl = 'http://sc-dev.jacynosoft.ovh';
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
