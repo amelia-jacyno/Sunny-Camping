bd0c342134f0c7e38d74fffcdd294722574e3bf8
jacynoadam <jacynoadam@gmail.com>
2021-05-06 21:33:24 +0200

    SC-88 add service_id to room and reservation

--- database/migrations/2021_05_06_185401_create_rooms_table.php
+++ database/migrations/2021_05_06_185401_create_rooms_table.php
@@ -15,6 +15,7 @@ class CreateRoomsTable extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
+            $table->integer('service_id');
             $table->string('name');
             $table->timestamps();
         });
