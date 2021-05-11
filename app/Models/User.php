a284e3f4ec217c67090c312d9b18d371361dbf17
amelia-jacyno <ameliajacyno@gmail.com>
2021-05-11 22:18:45 +0200

    SC-88 update PHPDocs and add empty Room Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,30 @@ use Illuminate\Notifications\Notifiable;
  * @method static Builder|User newQuery()
  * @method static Builder|User query()
  * @mixin Eloquent
+ * @property int $id
+ * @property int|null $role_id
+ * @property string $name
+ * @property string $email
+ * @property string|null $avatar
+ * @property string|null $password
+ * @property string $remember_token
+ * @property string|null $settings
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property mixed $locale
+ * @property-read \TCG\Voyager\Models\Role|null $role
+ * @property-read \Illuminate\Database\Eloquent\Collection|\TCG\Voyager\Models\Role[] $roles
+ * @property-read int|null $roles_count
+ * @method static Builder|User whereAvatar($value)
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereEmail($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereRoleId($value)
+ * @method static Builder|User whereSettings($value)
+ * @method static Builder|User whereUpdatedAt($value)
  */
 class User extends \TCG\Voyager\Models\User implements
     AuthenticatableContract,
