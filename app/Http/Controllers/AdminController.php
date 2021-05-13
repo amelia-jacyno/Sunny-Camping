19acbf3c96bab9103ba5e5bbda961229c8bff126
amelia-jacyno <ameliajacyno@gmail.com>
2021-05-13 21:02:37 +0200

    SC-88 add controller function for rooms and bungalows dashboards

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
+use App\Models\Service;
+
 class AdminController extends Controller
 {
     public function dashboard()
@@ -17,5 +20,23 @@ class AdminController extends Controller
     public function bills()
     {
         return view('admin.bills', ['page' => 'bills']);
+    }
+
+    public function rooms()
+    {
+        $rooms = Room::where('service_id', '=', Service::SERVICE_GUESTHOUSE)
+            ->with('reservations')
+            ->get();
+
+        // TODO: Pass rooms to a view
+    }
+
+    public function bungalows()
+    {
+        $bungalows = Room::where('service_id', '=', Service::SERVICE_BUNGALOWS)
+            ->with('reservations')
+            ->get();
+
+        // TODO: Pass bungalows to a view
     }
 }
