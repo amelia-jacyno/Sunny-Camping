ce147dc2625360968b5daf477a7189e4cc0b6894
PJ <pjacyn@gmail.com>
2021-05-17 21:14:55 +0200

    fix: move custom crud outside voyager, add voyager menu to custom crud panel

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,12 +1,29 @@
-@extends('voyager::master')
-@push('javascript')
+<html>
+<head>
+    @section('head')
+        <title>@yield('title', 'Słoneczny Camping - Wicie')</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}"/>
+        <link rel="stylesheet" href="{{mix('css/app.css')}}"/>
+        @stack('head')
+    @show
+</head>
+<body>
+<div id="app">
+    @section('navbar')
+        <custom-admin-menu :items="{{ menu('admin', '_json') }}"></custom-admin-menu>
+    @show
+    <div class="container">
+        @yield('main')
+    </div>
+</div>
+@section('scripts')
     <script>
         window.baseUrl = '{{ config('app.url') }}';
     </script>
     <script src="{{mix('js/app.js')}}"></script>
-@endpush
-@section('content')
-    <div id="app" class="container-fluid">
-        @yield('main')
-    </div>
-@endsection
+    @stack('scripts')
+@show
+</body>
+</html>
