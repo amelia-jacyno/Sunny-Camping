03cc2a78a53ae500e6c15b259aca44e46b460f04
amelia-jacyno <ameliajacyno@gmail.com>
2021-06-11 20:59:29 +0200

    hotfix: update prices and categories

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -16,10 +16,11 @@ class ItemSeeder extends Seeder
     {
         Item::factory()->createMany([
             ['service_category_id' => 1, 'name' => 'Dorosły', 'price' => 18],
-            ['service_category_id' => 1, 'name' => 'Dziecko', 'price' => 14],
-            ['service_category_id' => 2, 'name' => 'Duży', 'price' => 6],
-            ['service_category_id' => 2, 'name' => 'Mały', 'price' => 4],
-            ['service_category_id' => 3, 'name' => 'Prąd', 'price' => 10],
+            ['service_category_id' => 1, 'name' => 'Dziecko', 'price' => 15],
+            ['service_category_id' => 2, 'name' => 'Klimatyczne', 'price' => 2],
+            ['service_category_id' => 3, 'name' => 'Duży', 'price' => 7],
+            ['service_category_id' => 3, 'name' => 'Mały', 'price' => 5],
+            ['service_category_id' => 4, 'name' => 'Prąd', 'price' => 12],
         ]);
     }
 }
