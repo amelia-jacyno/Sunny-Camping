8c23ba2d802c8e5f41de0430faefe4c344d32f57
amelia-jacyno <ameliajacyno@gmail.com>
2021-06-13 18:46:53 +0200

    SC-4 add support for name search query

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
-use App\Repositories\ClientRepository;
+use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
@@ -12,9 +12,17 @@ class AdminController extends Controller
         return view('admin.dashboard', ['page' => 'dashboard']);
     }
 
-    public function clients()
+    public function clients(Request $request)
     {
-        $paginatedClients = Client::paginate(10);
+        $name = $request->input('name');
+
+        if ($name) {
+            $paginatedClients = Client::where([['name', 'LIKE', "%$name%"]])->paginate(10);
+        } else {
+            $paginatedClients = Client::paginate(10);
+        }
+
+        $paginatedClients->appends(\Request::except('page'));
 
         return view('admin.clients', [
             'page' => 'clients',
