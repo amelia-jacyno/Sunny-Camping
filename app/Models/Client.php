ff481619d51bc3630ce754aa39546d190eb51252
jacynoadam <jacynoadam@gmail.com>
2021-10-03 20:00:48 +0200

    SC-59 fix style

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Config;
 
 /**
  * App\Models\Client.
